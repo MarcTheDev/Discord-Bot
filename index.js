4c22e82f6bd12dae4fa0910c236321b71372aa84
MarcTheDev <marcthedev@gmail.com>
2018-04-22 15:53:24 -0700

    Added Protected Token

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-d
+const config = require('config.json');
+bot.on("message", async message => {
+
+});
+bot.token(config.token);
